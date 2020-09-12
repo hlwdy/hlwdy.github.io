cf2efa531fca70803fba88dc9d20387de330494f
hlwdy <2691948959@qq.com>
2020-09-12 11:31:40 +0800

    .

---
+++ map/map_files/init_map.js
@@ -0,0 +1,95 @@
+// 谷歌街道
+var GoogleNormalMap = L.tileLayer.chinaProvider('Google.Normal.Map', {
+        maxZoom: 23,
+        minZoom: 0
+    }),
+    // 地形
+    GoogleTerrainMap = L.tileLayer.chinaProvider('Google.Terrain.Map', {
+        maxZoom: 23,
+        minZoom: 0
+    }),
+    GoogleTerrainAnnotion = L.tileLayer.chinaProvider('Google.Terrain.Annotion', {
+        maxZoom: 23,
+        minZoom: 0
+    }),
+    // 卫星
+    GoogleSatelliteMap = L.tileLayer.chinaProvider('Google.Satellite.Map', {
+        maxZoom: 23,
+        minZoom: 0
+    }),
+    GoogleSatelliteAnnotion = L.tileLayer.chinaProvider('Google.Satellite.Annotion', {
+        maxZoom: 23,
+        minZoom: 0
+    });
+
+// 高德街道
+var GaoDeNormalMap = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
+        maxZoom: 18,
+        minZoom: 3
+    }),
+    // 卫星
+    GaoDeSatelliteMap = L.tileLayer.chinaProvider('GaoDe.Satellite.Map', {
+        maxZoom: 18,
+        minZoom: 0
+    }),
+    GaoDeSatelliteAnnotion = L.tileLayer.chinaProvider('GaoDe.Satellite.Annotion', {
+        maxZoom: 18,
+        minZoom: 0
+    });
+
+// Arcgis街道
+var ArcgisNormalMap = L.tileLayer.chinaProvider('Arcgis.Normal.Map', {
+        maxZoom: 18,
+        minZoom: 0
+    }),
+    // 卫星
+    ArcgisSatelliteMap = L.tileLayer.chinaProvider('Arcgis.Satellite.Map', {
+        maxZoom: 17,
+        minZoom: 0
+    }),
+    ArcgisSatelliteAnnotion = L.tileLayer.chinaProvider('Arcgis.Satellite.Annotion', {
+        maxZoom: 17,
+        minZoom: 0
+    });
+
+// Open街道
+var OpenNormalMap = L.tileLayer.chinaProvider('Open.Normal.Map', {
+    maxZoom: 23,
+    minZoom: 0,
+    id: 'mapbox.streets'
+});
+
+
+var google_streets = L.layerGroup([GoogleNormalMap]),
+    google_terrain = L.layerGroup([GoogleTerrainMap, GoogleTerrainAnnotion]),
+    google_satellite = L.layerGroup([GoogleSatelliteMap]),
+    google_hybrid = L.layerGroup([GoogleSatelliteMap, GoogleSatelliteAnnotion]);
+
+var gaode_streets = L.layerGroup([GaoDeNormalMap]),
+    gaode_satellite = L.layerGroup([GaoDeSatelliteMap]),
+    gaode_hybrid = L.layerGroup([GaoDeSatelliteMap, GaoDeSatelliteAnnotion]);
+
+var arcgis_streets = L.layerGroup([ArcgisNormalMap]),
+    arcgis_satellite = L.layerGroup([ArcgisSatelliteMap]),
+    arcgis_hybrid = L.layerGroup([ArcgisSatelliteMap, ArcgisSatelliteAnnotion]);
+
+var open_streets = L.layerGroup([OpenNormalMap]);
+
+var baseLayers = {
+	"街道地图": arcgis_streets,
+	"地形地图": google_terrain,
+    "卫星地图": arcgis_satellite,
+    "混合地图": arcgis_hybrid,
+};
+var overlayLayers = {};
+var map = L.map("map", {
+    center: [39.54, 116.23],
+    zoom: 8,
+    layers: [arcgis_streets],
+    zoomControl: true,
+    worldCopyJump: true,
+    zoomSnap: 0.5,
+    zoomDelta: 0.5,
+    wheelPxPerZoomLevel: 120
+});
+L.control.layers(baseLayers).addTo(map);
